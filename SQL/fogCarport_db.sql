516798641d1b24895376022240a4935f76a5e35b
Hassanain Ali <hassanainali786@hotmail.com>
2020-04-21 13:03:36 +0200

    tilføjet db

---
+++ SQL/fogCarport_db.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `orderId` int NOT NULL AUTO_INCREMENT,
+  `length` int NOT NULL,
+  `height` int NOT NULL,
+  `depth` int NOT NULL,
+  PRIMARY KEY (`orderId`)
+)
