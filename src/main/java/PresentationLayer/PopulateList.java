a63e71c43c14749eb128463d7836804baedc3304
KingBendico <cph-bc105@cphbusiness.dk>
2020-05-13 10:49:43 +0200

    lots of additions

---
+++ src/main/java/PresentationLayer/PopulateList.java
@@ -0,0 +1,32 @@
+package PresentationLayer;
+
+public class PopulateList {
+
+        private int id;
+        private String name;
+
+        public PopulateList(int id, String name) {
+            super();
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+
